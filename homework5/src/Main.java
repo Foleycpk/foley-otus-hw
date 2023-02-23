945fef0b7b79be10bb3c9be59ac8f5a927e65a63
Foley <foleycpk@gmail.com>
2023-02-23 20:50:05 +0300

    homework5 in process

--- homework5/src/Main.java
+++ homework5/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -6,20 +7,23 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = createList();
         print(list);
-        sort(list);
+        List<Integer> sortedList =  sort(list);
         print(list);
     }
 
     private static List<Integer> createList() {
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 5; i++) {
             list.add((int) (Math.random() * 7));
         }
         return list;
     }
 
-    private static void sort(List<Integer> list) {
+    private static List<Integer> sort(List<Integer> list) {
+        List<Integer> copyOfList = copyList(list);
+        print(copyOfList);
+
         int indexOfMin = -1;
 
         for (int i = 0; i < list.size() - 1; i++) {
@@ -36,8 +40,9 @@ public class Main {
                 list.set(indexOfMin, tmp);
             }
         }
+        return list;
     }
-    private static void print (List<Integer> list) {
+    private static void print(List<Integer> list) {
         for (int element : list) {
             System.out.print(element + " ");
         }
