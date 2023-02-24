89fd4a442fc0b124ea1288c7dc9441f7f72a54b2
Foley <foleycpk@gmail.com>
2023-02-24 22:56:53 +0300

    add sorting by Collections.sort()

--- homework5/src/Main.java
+++ homework5/src/Main.java
@@ -1,51 +1,63 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = createList();
         print(list);
+        long startTimeSorting = System.currentTimeMillis();
         List<Integer> sortedList =  sort(list);
-        print(list);
+        long endTimeSorting = System.currentTimeMillis();
+        print(sortedList);
+        System.out.println("Скорость работы сортировки методом выбора: " + (endTimeSorting - startTimeSorting));
+
+        startTimeSorting = System.currentTimeMillis();
+        Collections.sort(list);
+        endTimeSorting = System.currentTimeMillis();
+        System.out.println("Скорость работы сортировки с помощью : Collections.sort(): " + (endTimeSorting - startTimeSorting));
     }
 
     private static List<Integer> createList() {
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 1000; i++) {
             list.add((int) (Math.random() * 7));
         }
         return list;
     }
 
     private static List<Integer> sort(List<Integer> list) {
-        List<Integer> copyOfList = copyList(list);
-        print(copyOfList);
+        List<Integer> copyList = copyList(list);
 
         int indexOfMin = -1;
 
-        for (int i = 0; i < list.size() - 1; i++) {
-            int min = list.get(i);
-            for (int j = i; j < list.size(); j++) {
-                if (list.get(j) < min) {
-                    min = list.get(j);
+        for (int i = 0; i < copyList.size() - 1; i++) {
+            int min = copyList.get(i);
+            for (int j = i; j < copyList.size(); j++) {
+                if (copyList.get(j) < min) {
+                    min = copyList.get(j);
                     indexOfMin = j;
                 }
             }
-            if (min != list.get(i)) {
-                int tmp = list.get(i);
-                list.set(i, min);
-                list.set(indexOfMin, tmp);
+            if (min != copyList.get(i)) {
+                int tmp = copyList.get(i);
+                copyList.set(i, min);
+                copyList.set(indexOfMin, tmp);
             }
         }
-        return list;
+        return copyList;
     }
     private static void print(List<Integer> list) {
         for (int element : list) {
             System.out.print(element + " ");
         }
         System.out.println(" ");
+    }
+    
+    private static List<Integer> copyList(List<Integer> list) {
+        List<Integer> copyList = new ArrayList<>();
+        for (Integer element : list) {
+            copyList.add(element);
+        }
+        return copyList;
     }
 }
