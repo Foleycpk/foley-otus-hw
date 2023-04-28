c5e9cd3c9f9c5aa475644fe2fd157e8c0d3a5a37
Foley <foleycpk@gmail.com>
2023-04-28 22:13:53 +0300

    changed the test in the GameTestWithWinnerPlayer2 class from indirect to direct

--- homework8/homework/src/main/java/ru/otus/test/unit/fakes/GameWinnerPrinterFake.java
+++ homework8/homework/src/main/java/ru/otus/test/unit/fakes/GameWinnerPrinterFake.java
@@ -1,4 +1,18 @@
 package ru.otus.test.unit.fakes;
 
-public class GameWinnerPrinterFake {
+import ru.otus.game.GameWinnerPrinter;
+import ru.otus.game.Player;
+
+import java.util.List;
+
+public class GameWinnerPrinterFake implements GameWinnerPrinter {
+    private final List<String> realWinner;
+    public GameWinnerPrinterFake(List<String> realWinner) {
+        this.realWinner = realWinner;
+    }
+
+    @Override
+    public void printWinner(Player winner) {
+        realWinner.add(winner.getName());
+    }
 }
